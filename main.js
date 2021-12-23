3f3dee4ba542919628417394aae1c2f93df86ce0
배연준 <49051494+yjbae3974@users.noreply.github.com>
2021-12-23 16:30:31 +0900

    typescript까지 추가 완료

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-//formatting, Linting formating은 미적인거, linting은 혹여나 에러가 날 수 있는 것들.
-//formatting: Prettier 선호.
-//linting: ESLint
-//항상 lock파일을 커밋해줘야 협업하는 사람끼리 패키지 버전문제가 안생긴다.
+// @ts-check
 
-exports = 3;
+// formatting, Linting formating은 미적인거, linting은 혹여나 에러가 날 수 있는 것들.
+// formatting: Prettier 선호.
+// linting: ESLint
+// 항상 lock파일을 커밋해줘야 협업하는 사람끼리 패키지 버전문제가 안생긴다.
+
+const someString = 'hello'
+
+const result = Math.log(someString)
+
+console.log('hello')
+console.log(result)
