1eed31bfab60ba475814963053fce4f6ccb25675
배연준 <49051494+yjbae3974@users.noreply.github.com>
2021-12-23 16:36:40 +0900

    server init. added @types/node

--- main.js
+++ main.js
@@ -3,11 +3,19 @@
 // formatting, Linting formating은 미적인거, linting은 혹여나 에러가 날 수 있는 것들.
 // formatting: Prettier 선호.
 // linting: ESLint
+// typeChecking: Typescript
+
 // 항상 lock파일을 커밋해줘야 협업하는 사람끼리 패키지 버전문제가 안생긴다.
 
-const someString = 'hello'
+const http = require('http');
 
-const result = Math.log(someString)
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.end('Hello!')
+})
 
-console.log('hello')
-console.log(result)
+const PORT = 4000;
+
+server.listen(PORT,()=>{
+    console.log('The server is listening at',PORT);
+})
